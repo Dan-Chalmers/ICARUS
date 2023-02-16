b1b2a0cf7b3b5148e7244f3e7a85afd7c1acda44
Dan-Chalmers <55997584+Dan-Chalmers@users.noreply.github.com>
2023-02-16 09:35:49 +0000

    Added checking

--- Pixel Transformation/getPixelValues.py
+++ Pixel Transformation/getPixelValues.py
@@ -5,18 +5,25 @@ from PIL import Image as img
 import numpy as NP
 import glob
 import csv
+import os
 
 
 ''' Transform the images into numpy arrays, then dataframes, then write to xlsx '''
 def translate(directory: str, pxlDirectory: str):
 
     for image in glob.iglob(f'{directory}/*'):
-        shapeArray = NP.asarray(img.open(image))
-        processedShape = shapeArray.tolist()
+        
+        pathToCSV = pxlDirectory+'//{0}'.format(image.strip('.png').strip(directory))+'.csv'
+        if not os.path.exists(pathToCSV):
+            shapeArray = NP.asarray(img.open(image))
+            processedShape = shapeArray.tolist()
 
-        with open(pxlDirectory+'//{0}'.format(image.strip('.png').strip(directory))+'.csv', 'w', newline='') as pxlVals:
-            writer = csv.writer(pxlVals, delimiter=',')
-            writer.writerows(processedShape)
+            with open(pathToCSV, 'w', newline='') as pxlVals:
+                writer = csv.writer(pxlVals, delimiter=',')
+                writer.writerows(processedShape)
+
+        else:
+            print ('void')
 
 
 def main():
